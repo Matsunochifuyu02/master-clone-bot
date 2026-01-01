35880f3064c6147d20389a730839c192ada176bd
Matsunochifuyu02 <pintujawale423@gmail.com>
2026-01-01 11:40:05 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,3 +4,7 @@ API_ID = int(os.getenv("API_ID"))
 API_HASH = os.getenv("API_HASH")
 MASTER_BOT_TOKEN = os.getenv("MASTER_BOT_TOKEN")
 OWNER_ID = int(os.getenv("OWNER_ID"))
+
+# comma separated IDs → 123,456,789
+SUDO_USERS = list(
+    map(int, os.getenv("SUDO_USERS", "").split(",")))
